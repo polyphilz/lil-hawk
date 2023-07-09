3a0dffb083c567bd68f2f030b254ebd8b8d52dd8
Rohan Bansal <rohanarunbansal@gmail.com>
2023-07-09 12:17:00 +0800

    Major style improvements and refactoring for clarity

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,18 @@
+const infoLink = document.getElementById("info-link");
+const infoTooltip = document.getElementById("info-tooltip");
+
+infoLink.addEventListener("mouseover", function () {
+  infoTooltip.classList.remove("hidden");
+});
+
+infoLink.addEventListener("mouseout", function () {
+  infoTooltip.classList.add("hidden");
+});
+
+infoLink.addEventListener("click", function (event) {
+  event.preventDefault();
+});
+
 const wittyStatements = [
   "Unleashing our AI legal eagle...",
   "Analyzing with AI-powered x-ray vision...",
@@ -25,7 +40,8 @@ document.addEventListener("DOMContentLoaded", function () {
   let selectionModeCheckbox = document.getElementById("selection-mode-toggle");
   let tcInput = document.getElementById("tc-input");
   let openAiApiKeyInput = document.getElementById("openai-api-key");
-  let modelToggle = document.getElementById("gpt-toggle");
+  // let modelToggle = document.getElementById("gpt-toggle");
+  let modelRadios = document.getElementsByName("gpt-version");
   let submitButton = document.getElementById("submit-button");
   let egregiousnessTextArea = document.getElementById("egregious-likely");
 
@@ -49,6 +65,33 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  chrome.storage.local.get(["selectedModel"], (result) => {
+    modelRadios.forEach((radio) => {
+      if (radio.value === result.selectedModel) {
+        radio.checked = true;
+      }
+
+      radio.addEventListener("change", function () {
+        if (this.checked) {
+          const selectedModel = this.value;
+          chrome.storage.local.set({ selectedModel }, function () {
+            if (chrome.runtime.lastError) {
+              console.error(
+                "Error setting value to chrome storage: ",
+                chrome.runtime.lastError
+              );
+            } else {
+              console.log(
+                "Saved selectedModel to chrome storage: ",
+                selectedModel
+              );
+            }
+          });
+        }
+      });
+    });
+  });
+
   chrome.storage.local.get(["submitInFlight"], (result) => {
     submitButton.disabled = result.submitInFlight || false;
 
@@ -65,17 +108,23 @@ document.addEventListener("DOMContentLoaded", function () {
       chrome.storage.local.set({ submitInFlight: true }, () => {
         submitButton.disabled = true;
 
-        let model = modelToggle.checked ? "gpt-4" : "gpt-3.5-turbo";
-        let prompt = `Please highlight any content in the following legal text that is overly "egregious" (i.e. has anti-consumer privacy policies, harvests too much data or any other sort of potentially nefarious terms and conditions). Feel free to also highlight amusing, funny, generally interesting and/or "outrageous" clauses:\n\n${tcInput.value}?`;
-        chrome.runtime.sendMessage({
-          message: "sendToOpenAIChatAPI",
-          data: { model, prompt, apiKey },
-        });
+        chrome.storage.local.get(["selectedModel"], (result) => {
+          console.log(result.selectedModel);
+          let model = result.selectedModel;
+          let prompt = `Please highlight any content in the following legal text that is overly "egregious" (i.e. has anti-consumer privacy policies, harvests too much data or any other sort of potentially nefarious terms and conditions). Feel free to also highlight amusing, funny, generally interesting and/or dystopian clauses. Next to each highlight, add a short summary of why you believe that clause could be considered problematic. Return the results as a list of bullet points with 2 newlines in between:\n\n${tcInput.value}?`;
+          chrome.runtime.sendMessage({
+            message: "sendToOpenAIChatAPI",
+            data: { model, prompt, apiKey },
+          });
 
-        let randomIndex = Math.floor(Math.random() * wittyStatements.length);
-        const randomStatement = wittyStatements[randomIndex];
-        chrome.storage.local.set({ egregiousContent: randomStatement }, () => {
-          egregiousnessTextArea.value = wittyStatements[randomIndex];
+          let randomIndex = Math.floor(Math.random() * wittyStatements.length);
+          const randomStatement = wittyStatements[randomIndex];
+          chrome.storage.local.set(
+            { egregiousContent: randomStatement },
+            () => {
+              egregiousnessTextArea.value = wittyStatements[randomIndex];
+            }
+          );
         });
       });
     });
